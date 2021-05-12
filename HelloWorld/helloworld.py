3287c3c2d257eb5f7b6c2c24e31c4bdd28c43ee1
Daniel Kim <kkdh1109@gmail.com>
2021-05-12 18:35:59 -0400

    install python and create a first file

---
+++ HelloWorld/helloworld.py
@@ -0,0 +1,16 @@
+print('Hello, World!')
+greeting = 'Hello'
+# name = input('Please enter your name')
+name = 'Kim'
+
+print(greeting + ' ' + name)
+
+splitString= 'first line\n2nd line\n 3rd line'
+
+tabbedString = '1\t2\t3\t4'
+
+print(splitString)
+print(tabbedString)
+
+print("""Can write ' and ", and 
+skip lines using triple double quotes""")
