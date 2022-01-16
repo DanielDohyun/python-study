d3ae3e5e16525f3c4b4ccff370693569ee885128
Daniel Kim <kkdh1109@gmail.com>
2022-01-15 22:41:41 -0500

    python splice method

--- HelloWorld/helloworld.py
+++ HelloWorld/helloworld.py
@@ -14,3 +14,16 @@ print(tabbedString)
 
 print("""Can write ' and ", and 
 skip lines using triple double quotes""")
+
+# splicing
+# up to but not including the 2nd number
+parrot = 'Norwegian Blue'
+print(parrot[0:6])
+
+# can do without first value since by default it start from 0 index but still need to put :
+print(parrot[:6])
+
+print(parrot[3:5])
+
+print(parrot[10:14]) # returns Blue
+print(parrot[10:])
